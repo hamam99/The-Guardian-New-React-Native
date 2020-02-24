f15d91e7e025aece646d4471336b910d3f198e90
Hamam Nasrodin <zero.saturnus@gmail.com>
2020-02-25 00:34:05 +0700

    list_news/set up react navigation

---
+++ TheGuardianRN/src/screens/ListNews.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class ListNews extends Component {
+    render () {
+        return(
+            <View>
+                <Text>LIST NEWS</Text>
+            </View>
+        )
+    }
+}
+
+export default ListNews
